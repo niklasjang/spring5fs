1dba1fee907d02ac1eb36cff97c51cb0bac6f501
niklasjang <niklasjang@gmail.com>
2020-07-04 15:11:28 +0900

    Greeter class singleton test
    until page 51

--- sp5-chap02/src/main/java/chap02/Main.java
+++ sp5-chap02/src/main/java/chap02/Main.java
@@ -13,9 +13,9 @@ public class Main {
 		 * param1 : @Bean annotation의 메서드 이름(빈 객체의 이름)
 		 * param2 : 빈 객체의 타입
 		 * */
-		Greeter g = ctx.getBean("greeter", Greeter.class);
-		String msg = g.greet("스프링");
-		System.out.println(msg);
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		Greeter g2 = ctx.getBean("greeter", Greeter.class);
+		System.out.println("(g1 == g2) = " + (g1 == g2));
 		ctx.close();
 		
 	}
