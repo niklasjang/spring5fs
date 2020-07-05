8f24500f04ccc8e549b77519d61a99e11dab0284
niklasjang <niklasjang@gmail.com>
2020-07-05 13:24:17 +0900

    Three ways to cover nullable @Autowired
    until page 125

--- sp5-chap04/src/main/java/spring/MemberInfoPrinter.java
+++ sp5-chap04/src/main/java/spring/MemberInfoPrinter.java
@@ -25,8 +25,31 @@ public class MemberInfoPrinter {
 
 	@Autowired
 	@Qualifier("printer")
-	public void setPrinter(MemberPrinter printer) {
+	public void setPrinter(MemberSummaryPrinter printer) {
 		this.printer = printer;
 	}
-
+//	@Autowired될 의존 객체가 null일 수 있는 경우 대응 방법 3가지
+//	1.
+//	@Autowired(required=false)
+//	@Qualifier("printer")
+//	public void setPrinter(MemberSummaryPrinter printer) {
+//		this.printer = printer;
+//	}
+//	2.
+//	@Autowired
+//	public void setDateFormatter(Optional<DataTimeFormatter> formatterOpt){
+//	    if(forammterOpt.isPresent()){
+//	      this.dateTimeFormatter = formatterOpt.get();
+//	    }else{
+//	      this.dateTimeFormatter = null;
+//	      //물론 null에 대한 처리가 추가되어야 한다.
+//	    }
+//	}
+//	3. 
+//	@Autowired
+//	public void setDateFormatter(@Nullable DataTimeFormatter dateTimeFormatter){
+//	      this.dateTimeFormatter = null;
+//	      //물론 null에 대한 처리가 추가되어야 한다.
+//	    }
+//	}
 }
